b6613292686b42f8a8c44c40c65764857fdc1545
shejaemeric2 <shejaemeric2@gmail.com>
2023-02-08 15:39:23 +0200

    fix eslint error

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 function getListStudents() {
   return [
-    { firstName: "Guillaume", id: 1, location: "San Francisco" },
-    { firstName: "James", id: 2, location: "Columbia" },
-    { firstName: "Serena", id: 3, location: "San Francisco" },
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
   ];
 }
 export default getListStudents;
